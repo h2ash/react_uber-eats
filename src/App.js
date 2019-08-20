8a164bd36c2e3a817bd40a9911ffc64a61ead075
h2ash <hun22ash@gmail.com>
2019-08-21 00:35:27 +0300

    base header was implemented

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
+
+const BASE_URL = '';
+const IMG_URL = 'https://d3i4yxtzktqr9n.cloudfront.net/web-eats-v2';
 
 const App = () => (
   <div className="App">
-    <h1>React Uber eats</h1>
+    <header className='header'>
+      <div className='container header__container'>
+        <a href="" className='header__logo-link'>
+          <img 
+            className='header__logo-image'
+            src={`${IMG_URL}/f8f0721f871b3704cce92eb96bc6e504.svg`}
+            alt="Logo"
+          />
+        </a>
+  
+        <button className='header__location'>Location</button>
+        <button className='header__delivery-time'>Delivery now</button>
+        <button className='header__search'>Search</button>
+        <a href="" className='header__sign-in'>Sign</a>
+      </div>
+    </header>
   </div>
 );
 
