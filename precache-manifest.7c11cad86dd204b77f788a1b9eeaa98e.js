e96a43c5a7fc8845865d4b1d122acd5df1432d2f
h2ash <hun22ash@gmail.com>
2019-08-21 00:45:44 +0300

    Updates

--- precache-manifest.7c11cad86dd204b77f788a1b9eeaa98e.js
+++ precache-manifest.7c11cad86dd204b77f788a1b9eeaa98e.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "85071e27399260ecefa923f7ea177676",
+    "revision": "d35a710b249d3ea2913c1aed9a80dd70",
     "url": "/react_uber-eats/index.html"
   },
   {
-    "revision": "0b742b5127f0b3a2c6a3",
-    "url": "/react_uber-eats/static/css/main.65938976.chunk.css"
+    "revision": "7188b1aacdfea11731aa",
+    "url": "/react_uber-eats/static/css/main.a06c636f.chunk.css"
   },
   {
     "revision": "b5c5beb43fab1ff7f479",
     "url": "/react_uber-eats/static/js/2.ec295295.chunk.js"
   },
   {
-    "revision": "0b742b5127f0b3a2c6a3",
-    "url": "/react_uber-eats/static/js/main.c52d50dd.chunk.js"
+    "revision": "7188b1aacdfea11731aa",
+    "url": "/react_uber-eats/static/js/main.e86d7ae1.chunk.js"
   },
   {
     "revision": "183d5977488a46d51899",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react_uber-eats/precache-manifest.4831411afdd49c016f287f8976f8d4d0.js"
+  "/react_uber-eats/precache-manifest.7c11cad86dd204b77f788a1b9eeaa98e.js"
 );
 
 self.addEventListener('message', (event) => {
